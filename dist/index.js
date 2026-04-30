fffc99ba37253e124a83783977961d488336313f
johannesalke <johannesalke@gmail.com>
2026-04-30 02:12:32 +0200

    Started the project proper. bootdev run c4c8a3b8-161a-45f5-a75d-56030c187f68

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,22 @@
 import express from "express";
 const app = express();
 const PORT = 8080;
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponse);
+app.get("/healthz", handlerReadiness);
+function handlerReadiness(req, res) {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send("OK");
+}
+function middlewareLogResponse(req, res, next) {
+    res.on("finish", () => {
+        const stat = res.statusCode;
+        if (stat >= 300) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${stat}`);
+        }
+    });
+    next();
+}
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
-import express from "express";
+import express, { NextFunction } from "express";
+import { Request, Response } from "express";
+
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponse)
+app.get("/healthz", handlerReadiness);
+
+
+
+function handlerReadiness(req: Request, res: Response) {
+    res.set("Content-Type", "text/plain; charset=utf-8")
+    res.send("OK")
+
+}
+
+
+function middlewareLogResponse(req: Request, res: Response, next: NextFunction) {
+    res.on("finish", () => {
+        const stat = res.statusCode
+        if (stat >= 300) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${stat}`)
+        }
+    })
+    next()
+}
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
